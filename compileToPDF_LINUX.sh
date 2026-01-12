2f29bdc41ca38bcc8b2ebf8480c2854c7c32a5bc
MrKahr <madskhebsgaard@gmail.com>
2026-01-12 19:40:48 +0100

    FIX: slight improvement of build script features

--- compileToPDF_LINUX.sh
+++ compileToPDF_LINUX.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-FILE=main
+declare -r FILE=main
 
-pdflatex $FILE
-bibtex $FILE
-pdflatex $FILE
+compile(){
+    echo "\n--- Compiling source file ---\n"
+    pdflatex $FILE;
+    echo "--- Compiling bibliography file ---"
+    bibtex $FILE;
+    echo "--- Compiling source file ---"
+    pdflatex $FILE;
+    echo "--- Compilation successful ---"
+}
+
+compile
